43e88816a635dcef8d8b5f1886c6e9b66d1bacf0
Alex Klimovich <flautist1988@yandex.by>
2015-04-07 00:16:35 +0300

    init

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: { 
+      less: {
+        files: ['stylesheets/**/*.less'],
+        tasks: ['less']
+      },
+      jade: {
+        files: ['templates/**/*.jade'],
+        tasks: ['jade']
+      },
+      options: {
+        livereload: true
+      }
+    },
+    less: {
+      compile: {
+        files: {
+          "stylesheets/signUp.css": ["stylesheets/signUp.less"]
+        },
+        compress: false,
+        ieCompat: false,
+        relativeUrls: false
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: true
+          },
+          pretty: true
+        },
+        files: [{
+          cwd: 'templates',
+          src: ['**/*.jade', '!**/common/*.jade'],
+          dest: '.',
+          expand: true,
+          ext: '.html'
+        }]
+          
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+};
