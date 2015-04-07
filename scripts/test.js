1957d269f15f007f61500d08654642b0aeef1d52
Alex Klimovich <flautist1988@yandex.by>
2015-04-08 00:36:38 +0300

    #1 signUp photo

---
+++ scripts/test.js
@@ -0,0 +1,10 @@
+$('.sign-up-form .head .button-info').click(function(){
+  console.log(this.className +' clicked');
+});
+
+$('.sign-up-form .head .button-back').click(function(){
+  console.log(this.className +' clicked');
+});
+$('.sign-up-form .photo .photo').click(function(){
+  console.log(this.className +' clicked');
+});
