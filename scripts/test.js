e6ba3d10df205eee971ccae3a66176a2be88f41d
Alex Klimovich <flautist1988@yandex.by>
2015-04-10 00:53:08 +0300

    #1 signUp fields and button

--- scripts/test.js
+++ scripts/test.js
@@ -8,3 +8,7 @@ $('.sign-up-form .head .button-back').click(function(){
 $('.sign-up-form .photo .photo').click(function(){
   console.log(this.className +' clicked');
 });
+$('.sign-up-form .button button').click(function(e){
+  e.preventDefault();
+  console.log(this.className +' clicked');
+});
