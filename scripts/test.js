8022fcb768890509442838b49a34f1dfebdac9e6
Alex Klimovich <flautist1988@yandex.by>
2015-04-11 16:31:44 +0300

    #1 signUp | fixed photo click area, temporary disabled scripts

--- scripts/test.js
+++ scripts/test.js
@@ -1,14 +1,15 @@
 $('.sign-up-form .head .button-info').click(function(){
-  console.log(this.className +' clicked');
+  console.log('"' + this.className + '"' +' clicked');
 });
 
 $('.sign-up-form .head .button-back').click(function(){
-  console.log(this.className +' clicked');
+  console.log('"' + this.className + '"' +' clicked');
 });
-$('.sign-up-form .photo .photo').click(function(){
-  console.log(this.className +' clicked');
+$('#add-photo').click(function(e){
+  e.preventDefault();
+  console.log('"' + this.id + '"' + ' clicked');
 });
 $('.sign-up-form .button button').click(function(e){
   e.preventDefault();
-  console.log(this.className +' clicked');
+  console.log('"' + this.className + '"' +' clicked');
 });
