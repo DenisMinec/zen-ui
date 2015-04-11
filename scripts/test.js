8c5e58e0c792cf37807a53a21316f3125e8fa9d8
Alex Klimovich <flautist1988@yandex.by>
2015-04-11 19:14:50 +0300

    #1 signUp | SignUp link

--- scripts/test.js
+++ scripts/test.js
@@ -11,11 +11,15 @@ $('.sign-up-form .photo .add-photo').click(function(e){
   e.preventDefault();
   console.log('"' + this.className + '"' + ' clicked');
 });
-$('.sign-up-form .button button').click(function(e){
+$('.sign-up-form .button .sign-up').click(function(e){
   e.preventDefault();
   console.log('"' + this.className + '"' + ' clicked');
 });
 $('.sign-up-form .fields .show').click(function(e){
+  e.preventDefault();
+  console.log('"' + this.className + '"' + ' clicked');
+});
+$('.sign-up-form .sign-in .sign-in').click(function(e){
   e.preventDefault();
   console.log('"' + this.className + '"' + ' clicked');
 });
